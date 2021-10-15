530e110329648d693dc8785fdebab5080a74ac48
Sam Barrett <samuelbarrett1234@btinternet.com>
2021-10-15 10:05:14 +0100

    Moved functionality out of the class for easier testing.

--- compressors/chain.py
+++ compressors/chain.py
@@ -12,6 +12,82 @@ import tempfile
 from compressors.base import Compressor
 
 
+def _shuffle_file_lines(file, buffer_sz=1024*8):
+    """(Approximately) shuffle the lines of a (potentially very large) file.
+
+    Args:
+        file (file handle): The file to shuffle.
+        buffer_sz (int): The max number of lines we are allowed to read in at a time.
+    """
+    end = False
+    while not end:
+        buffer = []
+        pos = file.tell()
+        for i in range(buffer_sz):
+            s = file.readline()
+            if s != '':
+                buffer.append(s)
+            else:  # eof
+                end = True
+                break
+        random.shuffle(buffer)
+        file.seek(pos)
+        file.write("\n".join(buffer) + "\n")  # should be identical size in binary
+
+
+def _lazy_iterate(compressor, old_iter, shuffle):
+    """Create a new iterator from an old iterator by applying
+    the given compressor to all elements. To save on computation
+    the results are stored in a temporary file which is closed
+    when the return result of this function is garbage collected.
+    But also, since this iterator may never be needed, we do it
+    lazily to prevent excessive computation.
+
+    Args:
+        compressor (Compressor): The compressor to apply.
+        old_iter (Nullary function): Calling this creates an iterator
+                                     over integer list sequences.
+        shuffle (bool): If true, shuffle the data every time the
+                        iterator is created, else preserve order of
+                        the old iterator.
+
+    Returns:
+        A new iterator.
+    """
+
+    # unfortunately we must rely on the garbage collection of
+    # `new_iter` to clean this temporary file up, because while
+    # `new_iter` exists we could always receive another call
+    # to it.
+    storage = tempfile.TemporaryFile(mode='w+', newline='\n')
+
+    def new_iter():
+        storage.seek(0, os.SEEK_END)
+
+        if storage.tell() != 0:
+            # file is nonempty; don't need `old_iter`, just go
+            # through the lines of `storage`
+            storage.seek(0)
+
+            if shuffle:
+                # over time, this approximate shuffle will get better
+                # and better, because it overwrites the file each time.
+                _shuffle_file_lines(storage)
+
+            for line in storage.readline():
+                yield list(map(int, line.split()))
+
+        else:
+            # go through the old iterator only this once, writing
+            # the sequences as we see them, after compressing
+            for s in old_iter():
+                s = compressor.compress(s)
+                storage.write(" ".join(map(str, s)) + "\n")
+                yield s
+
+    return new_iter
+
+
 class CompressorChain:
     def __init__(self, compressors):
         self.compressors = compressors
@@ -28,8 +104,8 @@ class CompressorChain:
             # compressors even use `train`!
             # therefore we only want to put all of this data into a
             # file when it is called, not eagerly.
-            iter_train = self._lazy_iterate(c, iter_train, True)
-            iter_val = self._lazy_iterate(c, iter_val, False)
+            iter_train = _lazy_iterate(c, iter_train, True)
+            iter_val = _lazy_iterate(c, iter_val, False)
 
         return alphabet_size
 
@@ -46,55 +122,3 @@ class CompressorChain:
         for c in self.compressors:
             seqs = c.compressmany(seqs)
         return seqs
-
-
-    def _lazy_iterate(self, compressor, old_iter, shuffle):
-        # unfortunately we must rely on the garbage collection of
-        # `new_iter` to clean this temporary file up, because while
-        # `new_iter` exists we could always receive another call
-        # to it.
-        storage = tempfile.TemporaryFile(mode='w+', newline='\n')
-
-        def new_iter():
-            storage.seek(0, os.SEEK_END)
-
-            if storage.tell() != 0:
-                # file is nonempty; don't need `old_iter`, just go
-                # through the lines of `storage`
-                storage.seek(0)
-
-                if shuffle:
-                    # over time, this approximate shuffle will get better
-                    # and better, because it overwrites the file each time.
-                    self._shuffle_file_lines(storage)
-
-                for line in storage.readline():
-                    yield list(map(int, line.split()))
-
-            else:
-                # go through the old iterator only this once, writing
-                # the sequences as we see them, after compressing
-                for s in old_iter():
-                    s = compressor.compress(s)
-                    storage.write(" ".join(map(str, s)) + "\n")
-                    yield s
-
-        return new_iter
-
-
-    def _shuffle_file_lines(self, file):  # (approximately) shuffle the lines of a (large) file
-        buffer_sz = 1024 * 8  # number of lines to read per chunk
-        end = False
-        while not end:
-            buffer = []
-            pos = file.tell()
-            for i in range(buffer_sz):
-                s = file.readline()
-                if s != '':
-                    buffer.append(s)
-                else:  # eof
-                    end = True
-                    break
-            random.shuffle(buffer)
-            file.seek(pos)
-            file.write("\n".join(buffer))  # should be identical size in binary
