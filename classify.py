389065112bb4ea0dd53ca98d19249549a0c75967
Sam Barrett <samuelbarrett1234@btinternet.com>
2021-10-15 07:29:13 +0100

    Implemented classification script.

--- classify.py
+++ classify.py
@@ -5,11 +5,42 @@ this script uses different classification methods to classify the labels.
 
 
 import os
+import itertools
 import argparse as ap
+import sqlite3 as sql
+import progressbar as pgb
+
+
+def nearest_neighbour(db):
+    cur = db.cursor()
+    cur.execute("""
+    WITH TrainingPairings AS (
+        SELECT seqid_left AS seqid_train, seqid_right AS seqid_other,
+        lbltype, lbl, ncd_formula, ncd_value, compid
+        FROM SequencePairings JOIN Sequences ON seqid_left = Sequences.seqid
+        WHERE Sequences.seqpart = 0
+        JOIN NCDValues ON seqid_out = NCDValues.seqid
+        JOIN Labels ON Sequences.seqid = Labels.seqid
+        UNION ALL
+        SELECT seqid_right AS seqid_train, seqid_left AS seqid_other,
+        lbltype, lbl, ncd_formula, ncd_value, compid
+        FROM SequencePairings JOIN Sequences ON seqid_right = Sequences.seqid
+        WHERE Sequences.seqpart = 0
+        JOIN NCDValues ON seqid_out = NCDValues.seqid
+        JOIN Labels ON Sequences.seqid = Labels.seqid
+    ),
+    Labellings AS (
+        SELECT lbltype, ncd_formula, compid, seqid_other AS seqid, lbl, MIN(ncd_value)
+        FROM TrainingPairings
+        GROUP BY seqid_other, lbltype, ncd_formula, compid
+    )
+    SELECT lbltype, ncd_formula, compid, seqid, lbl FROM Labellings
+    """)
+    return cur.fetchall()
 
 
 CLASSIFICATION_METHODS = {
-    
+    '1-NN': nearest_neighbour
 }
 
 
@@ -23,4 +54,20 @@ if __name__ == "__main__":
         print("Error: cannot find ", args.db)
         exit(-1)
 
-    raise NotImplementedError()
+    db = sql.connect(args.db)
+
+    def apply(name_runner):
+        name, runner = name_runner
+        for row in runner(db):
+            yield (name,) + row
+
+    cur = db.cursor()
+    cur.executemany("""
+    INSERT INTO Predictions(predictor, lbltype, ncd_formula, compid, seqid, lbl)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """,
+    pgb.progressbar(itertools.chain(map(apply, CLASSIFICATION_METHODS.items())))
+    )
+
+    db.commit()
+    db.close()
