03aad51e02b46a3edb183b454b014fb6b94af21a
Sam Barrett <samuelbarrett1234@btinternet.com>
2021-10-15 09:54:14 +0100

    Created compressor ABC and the compressor chain.

--- compressors/base.py
+++ compressors/base.py
@@ -1,2 +1,65 @@
-class Compressor:
-    pass
+import abc
+
+
+class Compressor(abc.ABC):
+    @abc.abstractmethod
+    def train(self, alphabet_size, iter_train, iter_val):
+        """This is called before `compress` is ever called, to allow
+        the compressor to prepare. In particular, this tells the
+        compressor about the alphabet to expect. These will all be
+        integers in the range [0, alphabet_size).
+
+        This function can print whatever it wants, but if it wants any of
+        it to be persistent, this function should implement its own logging.
+
+        Args:
+            alphabet_size (int): The range of tokens to compress.
+            iter_train (nullary function): Calling this creates an iterator
+                                           which yields integer sequences
+                                           corresponding to the training set
+                                           in a newly shuffled order.
+            iter_val (nullary function): Calling this creates an iterator
+                                         which yields integer sequences
+                                         corresponding to the validation set
+                                         (not shuffled).
+
+        Returns:
+            The alphabet size of the output of this compressor.
+        """
+        pass
+
+
+    @abc.abstractmethod
+    def compress(self, seq):
+        """Compress a single integer sequence of tokens.
+
+        Args:
+            seq (list of int): The sequence to compress.
+        
+        Returns:
+            The compressed sequence, as a list of ints, but possibly with
+            a different alphabet size, as dictated by the return value of
+            `train`.
+        """
+        pass
+
+
+    @abc.abstractmethod
+    def compressmany(self, seqs):
+        """Compress a collection of integer sequences. Base classes
+        may be able to optimise this method, either on the GPU
+        or with thread pooling for large amounts of sequences.
+        However, obviously, the order of the output must equal the
+        order of input, because otherwise there will be no way to
+        associate the compressed sequences with the originals.
+
+        Args:
+            seqs (Iterator over integer sequences): A list, or generic iterator,
+                                                    over a (potentially very large)
+                                                    selection of 
+
+        Returns:
+            An iterator over (the compressed) integer sequences.
+            As discussed above, the output alphabet size may be different.
+        """
+        return map(self.compress, seqs)
--- compressors/chain.py
+++ compressors/chain.py
@@ -1,7 +1,100 @@
 """
 This compressor trains and runs a chain of compressors.
-It will have to use temporary storage (DB? Temporary file?) to
-store the intermediate compression results. Because in order to
-train the 2nd compressor you need the 1st compressor to have done
-a pass over all of the data.
+It uses an intermediate temporary file to store compression results.
+Because in order to train the 2nd compressor you need the 1st compressor
+to have trained over all of the data.
 """
+
+
+import os
+import random
+import tempfile
+from compressors.base import Compressor
+
+
+class CompressorChain:
+    def __init__(self, compressors):
+        self.compressors = compressors
+
+
+    def train(self, alphabet_size, iter_train, iter_val):
+        for c in self.compressors:
+            alphabet_size = c.train(alphabet_size, iter_train, iter_val)
+
+            # now we need to update `iter_train` and `iter_val`.
+            # we could simply do this by mapping `c.compress` over them,
+            # but this is inefficient.
+            # however, another complication is the fact that not all
+            # compressors even use `train`!
+            # therefore we only want to put all of this data into a
+            # file when it is called, not eagerly.
+            iter_train = self._lazy_iterate(c, iter_train, True)
+            iter_val = self._lazy_iterate(c, iter_val, False)
+
+        return alphabet_size
+
+
+    def compress(self, seq):
+        for c in self.compressors:
+            seq = c.compress(seq)
+        return seq
+
+
+    def compressmany(self, seqs):
+        # cool fact: this function will often not perform any work,
+        # and will simply return an iterator over many composed compressors
+        for c in self.compressors:
+            seqs = c.compressmany(seqs)
+        return seqs
+
+
+    def _lazy_iterate(self, compressor, old_iter, shuffle):
+        # unfortunately we must rely on the garbage collection of
+        # `new_iter` to clean this temporary file up, because while
+        # `new_iter` exists we could always receive another call
+        # to it.
+        storage = tempfile.TemporaryFile(mode='w+', newline='\n')
+
+        def new_iter():
+            storage.seek(0, os.SEEK_END)
+
+            if storage.tell() != 0:
+                # file is nonempty; don't need `old_iter`, just go
+                # through the lines of `storage`
+                storage.seek(0)
+
+                if shuffle:
+                    # over time, this approximate shuffle will get better
+                    # and better, because it overwrites the file each time.
+                    self._shuffle_file_lines(storage)
+
+                for line in storage.readline():
+                    yield list(map(int, line.split()))
+
+            else:
+                # go through the old iterator only this once, writing
+                # the sequences as we see them, after compressing
+                for s in old_iter():
+                    s = compressor.compress(s)
+                    storage.write(" ".join(map(str, s)) + "\n")
+                    yield s
+
+        return new_iter
+
+
+    def _shuffle_file_lines(self, file):  # (approximately) shuffle the lines of a (large) file
+        buffer_sz = 1024 * 8  # number of lines to read per chunk
+        end = False
+        while not end:
+            buffer = []
+            pos = file.tell()
+            for i in range(buffer_sz):
+                s = file.readline()
+                if s != '':
+                    buffer.append(s)
+                else:  # eof
+                    end = True
+                    break
+            random.shuffle(buffer)
+            file.seek(pos)
+            file.write("\n".join(buffer))  # should be identical size in binary
