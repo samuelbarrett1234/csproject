b806c0e0076c0029b67350875231fe427cb72d1c
Sam Barrett <samuelbarrett1234@btinternet.com>
2021-11-17 08:01:51 +0000

    Added label scoring.

--- results.py
+++ results.py
@@ -8,6 +8,9 @@ import os
 import csv
 import argparse as ap
 import sqlite3 as sql
+import scipy.sparse
+import scipy.sparse.linalg
+import scipy.sparse.csgraph
 import progressbar as pgb
 
 
@@ -119,10 +122,68 @@ def compute_compression_ratios(db):
     )
 
 
+def compute_lbl_scores(db):
+    results = []
+
+    cur = db.cursor()
+    cur2 = db.cursor()
+
+    cur.execute("SELECT MAX(seqid) + 1 FROM Sequences WHERE seq_is_pair = 0")
+    N = cur.fetchone()[0]
+
+    # compute all one-hot label matrices
+    lbl_mats = {}
+    lbltype_names = {}
+    cur2.execute("SELECT lbltype, lbltype_name, MAX(lbl) + 1 FROM Labels "
+                 "NATURAL JOIN LabelTypes GROUP BY lbltype")
+    for lbltype, lbltype_name, M in cur2.fetchall():
+        cur.execute("SELECT seqid, lbl FROM Labels WHERE lbltype = ?",
+                    (lbltype,))
+        Y = scipy.sparse.dok_matrix((N, M))
+        for seqid, lbl in cur.fetchall():
+            Y[seqid, lbl] = 1.0
+        lbl_mats[lbltype] = Y
+        lbltype_names[lbltype] = lbltype_name
+
+    cur2.execute("SELECT DISTINCT compid, compname, ncd_formula "
+                 "FROM NCDValues NATURAL JOIN Compressors")
+    for compid, compname, ncd_formula in cur2.fetchall():
+        # NOTE: it is important that this query symmetrizes the `ncd_value`
+        # correctly
+        cur.execute("""
+            WITH MySequences AS (
+                SELECT seqid_left, seqid_right,
+                CASE WHEN seqid_left = seqid_right THEN 0.0 ELSE ncd_value
+                END AS ncd_value
+                FROM NCDValues NATURAL JOIN Sequences
+                JOIN SequencePairings ON Sequences.seqid = seqid_out
+                WHERE compid = ? AND ncd_formula = ? AND seqpart = 1
+            )
+            SELECT A.seqid_left, A.seqid_right, 0.5 * (A.ncd_value + B.ncd_value)
+            FROM MySequences AS A JOIN MySequences AS B
+            ON A.seqid_left = B.seqid_right AND A.seqid_right = B.seqid_left
+        """, (compid, ncd_formula))
+        D = scipy.sparse.dok_matrix((N, N))
+        for left, right, dist in cur.fetchall():
+            D[(left, right)] = max(dist, 0.0)  # clip to be >= 0
+        # compute Laplacian
+        D = scipy.sparse.csgraph.laplacian(D)
+        # now score each label type:
+        for lbltype, Y in lbl_mats.items():
+            s = sum((Y.T @ D @ Y).diagonal()) / Y.shape[1]
+            assert(s >= 0)
+            results.append(
+                (compname, ncd_formula, lbltype_names[lbltype], s)
+            )
+
+    return (('Compression_Algorithm', 'NCD_Formula', 'Label_Type', 'Score'), results)
+
+
 METRICS = {
     'accuracy': compute_accuracy,
     'sizes': compute_compression_sizes,
-    'ratios': compute_compression_ratios
+    'ratios': compute_compression_ratios,
+    'lbl_scores': compute_lbl_scores
 }
 
 
