a0f813da5ce613de0668e7759e3d4264e9c63390
Sam Barrett <samuelbarrett1234@btinternet.com>
2021-12-01 14:10:25 +0000

    Fixed some old tests.

--- masking/masking_test.py
+++ masking/masking_test.py
@@ -2,22 +2,12 @@ import numpy as np
 import masking
 
 
-def test_apply_mask():
-    seqs = np.array([[1, 2, 3, 0], [4, 5, 6, 7]], dtype=np.int32)
-    mask = (seqs != 0)
-    result = masking._apply_mask(
-        seqs, mask, 8, 8, 0
-    )
-    assert(result.shape == seqs.shape)
-    assert(result[0, 3] == 0)
-
-
 def test_bert_masking():
     result = masking.bert_masking(
         [np.array([1, 2, 3], dtype=np.int32),
          np.array([4, 5, 6, 7], dtype=np.int32)],
         0, 8, 8
-    )
+    )[0]
     assert(result.shape == (2, 4))
 
 
@@ -26,7 +16,7 @@ def test_span_bert_masking():
         [np.array([1, 2, 3], dtype=np.int32),
          np.array([4, 5, 6, 7], dtype=np.int32)],
         0, 8, 8
-    )
+    )[0]
     assert(result.shape == (2, 4))
 
 
@@ -41,5 +31,5 @@ def test_cut_sort_masking():
         [np.array([1, 2, 3], dtype=np.int32),
          np.array([4, 5, 6, 7], dtype=np.int32)],
         0, 8, 8
-    )
+    )[0]
     assert(result.shape == (2, 4))
